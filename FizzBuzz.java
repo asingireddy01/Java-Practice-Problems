d68481accc9d24b50dd971b9917ec96d9a9d50d0
asingireddy01 <95263507+asingireddy01@users.noreply.github.com>
2021-12-25 21:26:46 -0500

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class FizzBuzz
+{
+    public static void main(String[] args)
+    {
+        for (int i=5; i>=1; i--)
+            System.out.println(i + ", ");
+    }
+    System.out.println("Blastoff!");
+
+    }
+}
